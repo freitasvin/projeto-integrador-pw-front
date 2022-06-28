c9ac3d9adabd60f01373720f374366026d1d04f3
Vinicius Freitas <vinicius.freitas@ciss.com.br>
2022-06-28 14:18:10 -0300

    Refactor SingUp form

--- src/pages/Sing-up/styles/index.js
+++ src/pages/Sing-up/styles/index.js
@@ -1,31 +1,40 @@
 import styled from 'styled-components';
 
 export const ContainerStyle = styled.div`
-display: flex;
-flex-direction: row;
-height: 100vh;
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
 `;
 
 export const MainStyle = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-left: 50px;
-  padding-right: 200px;
-  margin-bottom: 2%;
+    font-weight: 500;
+    color: #03B7DA;
+    height: 100vh;
+    display: flex;
+    flex-basis: 75%;
+    flex-direction: column;
+    padding-left: 50px;
+    padding-right: 100px;
+    justify-content: space-around;
 
   h1 {
     color: #03B7DA;
     font-size: 20px;
     font-weight: 500;
     flex-basis: 100%;
+    margin-bottom: 20px;
   }
 `;
 
 export const FormStyled = styled.form`
-  flex-basis: 50%;
   display: flex;
   flex-wrap: wrap;
-  margin-top: 2%;
+  float: left;
+  align-items: center;
+
+  input {
+    margin-bottom: 15px;
+  }
 `;
 
 export const BreakStyle = styled.div`
